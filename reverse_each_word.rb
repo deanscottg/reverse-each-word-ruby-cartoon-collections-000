e2a60248cc53a1f371e77ed9561495f87202fc44
Dean Gelbaum <deanscottg@gmail.com>
2020-05-18 15:45:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 # string = "Hello there, and how are you"
-def reverse_each_word (sentence)
+# def reverse_each_word (sentence)
   new_array = []
        sentence.split.each do |word| 
       new_array.push(word.reverse)
@@ -8,7 +8,7 @@ def reverse_each_word (sentence)
   new_array.join(" ")
   end 
  
- def reverse_each_word_collect(sentence)
+ def reverse_each_word(sentence)
  sentence.split.collect do |word|
  word.reverse
 end
